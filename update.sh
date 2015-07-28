538077553fd8ba7ac71e3a15c48e58ab330f400e
NimbusScale <joe@nimbusscale.com>
2015-07-28 12:01:38 -0700

    with cf-init

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws s3 cp ./ s3://nimbusscale-cfn/ --recursive --exclude "*" --include "*.json"
+aws cloudformation create-stack --stack-name "network" --parameters ParameterKey="Prefix",ParameterValue="TEST-" --template-url "https://s3-us-west-2.amazonaws.com/nimbusscale-cfn/network.json" --capabilities CAPABILITY_IAM
+#aws cloudformation update-stack --stack-name "network" --parameters ParameterKey="Prefix",ParameterValue="TEST-" --template-url "https://s3-us-west-2.amazonaws.com/nimbusscale-cfn/network.json" --capabilities CAPABILITY_IAM
+
+
+
