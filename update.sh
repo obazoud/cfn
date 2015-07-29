7cb6a8ce569e61de35e6c3e3d545332c8078a0d1
NimbusScale <joe@nimbusscale.com>
2015-07-28 17:14:59 -0700

    updated to support multiple region

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws s3 cp ./ s3://nimbusscale-cfn/ --recursive --exclude "*" --include "*.json"
-aws cloudformation create-stack --stack-name "network" --parameters ParameterKey="Prefix",ParameterValue="TEST-" --template-url "https://s3-us-west-2.amazonaws.com/nimbusscale-cfn/network.json" --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name "network" --parameters ParameterKey="Prefix",ParameterValue="TEST-" ParameterKey="VpcIpPrefix",ParameterValue="192.168" --template-url "https://s3-us-west-2.amazonaws.com/nimbusscale-cfn/network.json" --capabilities CAPABILITY_IAM
 #aws cloudformation update-stack --stack-name "network" --parameters ParameterKey="Prefix",ParameterValue="TEST-" --template-url "https://s3-us-west-2.amazonaws.com/nimbusscale-cfn/network.json" --capabilities CAPABILITY_IAM
 
 
